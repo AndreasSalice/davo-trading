c4de42e19d18b483fbf3559a5aa13e2652197ec1
AndreasSalice <153663952+AndreasSalice@users.noreply.github.com>
2025-10-18 14:52:50 +0200

    responsiveness bug fix poging 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <div className="overflow-x-hidden w-full">
       <Navbar></Navbar>
       <Hero></Hero>
       <Propos></Propos>
